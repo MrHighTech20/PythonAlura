06266677a3e8f1eda3ebce94ad9898b0ec12a818
MrHighTech20 <marcoscantelli@hotmail.com>
2023-03-22 14:10:50 -0300

    implementando laco de rerpeticao com while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,22 +3,28 @@ print("Bem vindo ao jogo de Adivinhacao")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while(rodada <= total_de_tentativas):
 
-print("Voce digitou:", chute_str)
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Voce digitou:", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    if(acertou):
+        print("Voce acertou")
+    else:
+        if(maior):
+            print("Voce errou, seu chute foi maior que o numero secreto")
+        elif(menor):
+            print("Voce errou, su chute foi menor que o numero secreto")
 
-if(acertou):
-    print("Voce acertou")
-else:
-    if(maior):
-        print("Voce errou, seu chute foi maior que o numero secreto")
-    elif(menor):
-        print("Voce errou, su chute foi menor que o numero secreto")
+    rodada = rodada + 1
+
 print("Fim do jogo")
